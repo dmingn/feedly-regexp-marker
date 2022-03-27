8b282bdb5f814c2382fc37fb94102752176cc97a
dmingn <dmingn@users.noreply.github.com>
2022-03-27 09:16:30 +0900

    implement

---
+++ feedly_regexp_marker/__main__.py
@@ -0,0 +1,5 @@
+from feedly_regexp_marker.cli import main
+
+
+if __name__ == "__main__":
+    main()
---
+++ feedly_regexp_marker/cli.py
@@ -0,0 +1,47 @@
+import time
+from pathlib import Path
+from typing import Optional
+
+import click
+import schedule
+
+from feedly_regexp_marker.lib.classifier import Classifier
+from feedly_regexp_marker.lib.feedly_controller import FeedlyController
+
+
+@click.command()
+@click.option(
+    "--rules", type=click.Path(exists=True, path_type=Path), required=True
+)
+@click.option(
+    "--every-n-minutes",
+    type=click.IntRange(min=1),
+    default=None,
+)
+@click.option("-n", "--dry-run", is_flag=True)
+def main(rules: Path, every_n_minutes: Optional[int], dry_run: bool):
+    feedly_controller = FeedlyController()
+
+    def job():
+        entries = feedly_controller.fetch_unread_entries()
+
+        clf = Classifier.from_yaml(rules)
+
+        feedly_controller.save_entries(
+            entries=[entry for entry in entries if clf.to_save(entry)],
+            dry_run=dry_run,
+        )
+
+        feedly_controller.read_entries(
+            entries=[entry for entry in entries if clf.to_read(entry)],
+            dry_run=dry_run,
+        )
+
+    if every_n_minutes:
+        schedule.every(every_n_minutes).minutes.do(job)
+
+        while True:
+            schedule.run_pending()
+            time.sleep(1)
+    else:
+        job()
