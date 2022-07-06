e1c1b292122c12b17748586250c5842db7fb1750
dmingn <dmingn@users.noreply.github.com>
2022-07-07 00:43:47 +0900

    log

--- feedly_regexp_marker/cli.py
+++ feedly_regexp_marker/cli.py
@@ -6,6 +6,7 @@ from typing import Callable, Optional
 import click
 from feedly.api_client.protocol import UnauthorizedAPIError, WrappedHTTPError
 from feedly.api_client.session import FileAuthStore
+from logzero import logger
 from slack_sdk import WebhookClient
 from watchfiles import watch
 
@@ -15,15 +16,27 @@ from feedly_regexp_marker.lib.feedly_controller import FeedlyController
 access_token_path = Path.home() / ".config" / "feedly" / "access.token"
 
 
+def log_function_call(f: Callable) -> Callable:
+    def wrapper(*args, **kwargs):
+        logger.info(f"function {f.__name__} called with {args, kwargs}.")
+        ret = f(*args, **kwargs)
+        logger.info(f"function {f.__name__} returned {ret}.")
+        return ret
+
+    return wrapper
+
+
 def report_exception(client: WebhookClient) -> Callable:
     def report_exception(f: Callable) -> Callable:
         def wrapper(*args, **kwargs):
             try:
                 f(*args, **kwargs)
             except WrappedHTTPError as e:
+                logger.exception(e)
                 client.send(text=e.reason)
                 raise
             except BaseException as e:
+                logger.exception(e)
                 client.send(text=str(e))
                 raise
 
@@ -32,11 +45,13 @@ def report_exception(client: WebhookClient) -> Callable:
     return report_exception
 
 
+@log_function_call
 def wait_for_creation(path: Path):
     while not path.exists():
         time.sleep(1)
 
 
+@log_function_call
 def wait_for_change(path: Path):
     for _ in watch(path, rust_timeout=0):
         break
@@ -55,10 +70,13 @@ def sleep_and_repeat(minutes_to_sleep: Optional[int]) -> Callable:
                         if e.filename == str(access_token_path):
                             wait_for_creation(access_token_path)
                         else:
+                            logger.info(f"sleep for {minutes_to_sleep} minutes.")
                             time.sleep(minutes_to_sleep * 60)
                     except BaseException:
+                        logger.info(f"sleep for {minutes_to_sleep} minutes.")
                         time.sleep(minutes_to_sleep * 60)
                     else:
+                        logger.info(f"sleep for {minutes_to_sleep} minutes.")
                         time.sleep(minutes_to_sleep * 60)
             else:
                 f(*args, **kwargs)
@@ -79,6 +97,7 @@ def sleep_and_repeat(minutes_to_sleep: Optional[int]) -> Callable:
 def main(rules: Path, minutes_to_sleep: Optional[int], dry_run: bool):
     @sleep_and_repeat(minutes_to_sleep=minutes_to_sleep)
     @report_exception(client=WebhookClient(url=os.environ["SLACK_WEBHOOK_URL"]))
+    @log_function_call
     def job():
         feedly_controller = FeedlyController(auth=FileAuthStore())
 
