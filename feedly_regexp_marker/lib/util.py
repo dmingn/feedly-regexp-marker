cbdfdbc19e6c1b9493daee0de875b9b83ac54859
dmingn <dmingn@users.noreply.github.com>
2025-02-01 10:54:29 +0900

    stop logging function call

--- feedly_regexp_marker/lib/util.py
+++ feedly_regexp_marker/lib/util.py
@@ -5,16 +5,6 @@ from logzero import logger
 from slack_sdk import WebhookClient
 
 
-def log_function_call(f: Callable) -> Callable:
-    def wrapper(*args, **kwargs):
-        logger.info(f"function {f.__name__} called with {args, kwargs}.")
-        ret = f(*args, **kwargs)
-        logger.info(f"function {f.__name__} returned {ret}.")
-        return ret
-
-    return wrapper
-
-
 def report_exception(client: WebhookClient) -> Callable:
     def report_exception(f: Callable) -> Callable:
         def wrapper(*args, **kwargs):
