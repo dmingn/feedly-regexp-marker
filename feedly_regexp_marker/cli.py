bb9e64e0f6542fc388d386e6f72dd74cb9ada891
dmingn <dmingn@users.noreply.github.com>
2025-02-01 10:57:09 +0900

    stop reporting exception to slack

--- feedly_regexp_marker/cli.py
+++ feedly_regexp_marker/cli.py
@@ -1,14 +1,11 @@
-import os
 from pathlib import Path
 
 import click
 from feedly.api_client.session import FileAuthStore
 from logzero import logger
-from slack_sdk import WebhookClient
 
 from feedly_regexp_marker.lib.classifier import Classifier
 from feedly_regexp_marker.lib.feedly_controller import FeedlyController
-from feedly_regexp_marker.lib.util import report_exception
 
 access_token_path = Path.home() / ".config" / "feedly" / "access.token"
 
@@ -17,7 +14,6 @@ access_token_path = Path.home() / ".config" / "feedly" / "access.token"
 @click.option("--rules", type=click.Path(exists=True, path_type=Path), required=True)
 @click.option("-n", "--dry-run", is_flag=True)
 def main(rules: Path, dry_run: bool):
-    @report_exception(client=WebhookClient(url=os.environ["SLACK_WEBHOOK_URL"]))
     def job():
         feedly_controller = FeedlyController(auth=FileAuthStore())
 
